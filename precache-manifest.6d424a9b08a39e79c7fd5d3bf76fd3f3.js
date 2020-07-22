dfd459f7e12a7fbb375e657ae984c5f9617d1a6e
Danny Avery <danny.w.avery@gmail.com>
2020-07-22 16:54:54 -0500

    Updates

---
+++ precache-manifest.6d424a9b08a39e79c7fd5d3bf76fd3f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3bd50a2babff813f0e314aed6ccb7a",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "fcfc6c8f69ef023eaafe",
+    "url": "/pokedex/static/css/main.67ca31aa.chunk.css"
+  },
+  {
+    "revision": "ecb1a1fed1772b81a331",
+    "url": "/pokedex/static/js/2.422c94c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.422c94c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcfc6c8f69ef023eaafe",
+    "url": "/pokedex/static/js/main.9e4e722c.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
